5670391857f9e7762bfdbb179e5d48d650c72d4a
Jacob Brown <Jake@insphereltd.com>
2022-10-14 20:03:45 +0100

    Added basic math speed profile test.

---
+++ src/mathprofile/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "crtime.h"
+
+const int NUM_ITERATIONS = 1000000000;
+
+double int32_test()
+{
+	uint64_t start = GetTicks();
+
+	for (int i = 0; i < NUM_ITERATIONS; i++)
+	{
+		volatile int32_t add = 123 + 654;
+		volatile int32_t sub = 123 - 654;
+		volatile int32_t mul = 123 * 654;
+		volatile int32_t div = 123 / 654;
+	}
+
+	double time = GetTimeInMS(GetTicks() - start);
+
+	return GetTimeInMS(GetTicks() - start);
+}
+
+double int64_test()
+{
+	uint64_t start = GetTicks();
+
+	for (int i = 0; i < NUM_ITERATIONS; i++)
+	{
+		volatile int32_t add = 123 + 654;
+		volatile int32_t sub = 123 - 654;
+		volatile int32_t mul = 123 * 654;
+		volatile int32_t div = 123 / 654;
+	}
+
+	double time = GetTimeInMS(GetTicks() - start);
+
+	return GetTimeInMS(GetTicks() - start);
+}
+
+double float_test()
+{
+	uint64_t start = GetTicks();
+
+	for (int i = 0; i < NUM_ITERATIONS; i++)
+	{
+		volatile int32_t add = 123 + 654;
+		volatile int32_t sub = 123 - 654;
+		volatile int32_t mul = 123 * 654;
+		volatile int32_t div = 123 / 654;
+	}
+
+	double time = GetTimeInMS(GetTicks() - start);
+
+	return GetTimeInMS(GetTicks() - start);
+}
+
+double double_test()
+{
+	uint64_t start = GetTicks();
+
+	for (int i = 0; i < NUM_ITERATIONS; i++)
+	{
+		volatile int32_t add = 123 + 654;
+		volatile int32_t sub = 123 - 654;
+		volatile int32_t mul = 123 * 654;
+		volatile int32_t div = 123 / 654;
+	}
+
+	double time = GetTimeInMS(GetTicks() - start);
+
+	return GetTimeInMS(GetTicks() - start);
+}
+
+int main(int argc, char* argv)
+{
+	printf("Starting tests...\n\n");
+
+	printf("int32_t time:\t%f ms\n", int32_test());
+	printf("int64_t time:\t%f ms\n", int64_test());
+	printf("float time:\t%f ms\n", float_test());
+	printf("double time:\t%f ms\n", double_test());
+
+	printf("\nTests complete\n");
+	getchar();
+
+	return EXIT_SUCCESS;
+}
